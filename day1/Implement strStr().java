2eb5e5cd609dbd85d717e7532ff770a200a4087e
Atul Kr Karn <atul48kumar90@gmail.com>
2022-06-20 20:40:12 +0530

    day1

---
+++ day1/Implement strStr().java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle==null)
+        {
+            return 0;
+        }
+        else{
+             int index = haystack.indexOf(needle);
+            return index;
+        }
+    }
+}
