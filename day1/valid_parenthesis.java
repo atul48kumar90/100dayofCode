10e2efa1b1cbd878a78b5b9218a4ad9b7d556010
Atul Kr Karn <atul48kumar90@gmail.com>
2022-06-20 19:05:22 +0530

    day1

---
+++ day1/valid_parenthesis.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] x = s.toCharArray();
+        Stack stk = new Stack<>();
+        
+        for(int i=0;i<x.length;i++)
+        {
+            if(x[i] == '(' || x[i]=='{' || x[i]=='[')
+            {
+                stk.push(x[i]);
+            }
+            else if(stk.empty()==false && (x[i]==')' && stk.peek().equals('('))
+                   || ( x[i] == ']'  && stk.peek().equals('['))
+           || ( x[i] == '}'  && stk.peek().equals('{')))
+            {
+                stk.pop();
+            }
+            else
+        {
+            return false;
+        }
+    }
+    
+    if(stk.empty() == true){
+        return true;
+    }else{
+        return false;
+    }
+        
+    }
+}
