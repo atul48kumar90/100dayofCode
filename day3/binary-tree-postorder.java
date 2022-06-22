af760ca1f51dfaea254a54675b068adde8e82a68
Atul Kr Karn <atul48kumar90@gmail.com>
2022-06-22 22:48:05 +0530

    day3q3

---
+++ day3/binary-tree-postorder.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        if(root != null)
+        {
+            traverse(root, ls);
+        }
+        return ls;
+    }
+    
+    public void traverse(TreeNode root , ArrayList<Integer> ls)
+    {
+        if(root.left != null)
+        {
+            traverse(root.left, ls);
+        }
+        if(root.right != null)
+        {
+            traverse(root.right, ls);
+        }
+        ls.add(root.val);
+    }
+}
