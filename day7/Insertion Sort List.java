c1bd29939ef29b31d37eb818f5e5b0a85e8ff2b5
Atul Kr Karn <atul48kumar90@gmail.com>
2022-09-30 17:15:12 +0530

    Insertion Sort List

---
+++ day7/Insertion Sort List.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        
+        while(head != null)
+        {
+            ListNode temp = dummy;
+            ListNode next = head.next;
+            
+            while(temp.next != null && temp.next.val < head.val)
+            {
+                temp = temp.next;
+            }
+            
+                head.next = temp.next;
+                temp.next = head;
+                head = next;
+            
+        }
+        return dummy.next;
+    }
+}
